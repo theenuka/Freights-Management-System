1ff9a2447c74925623223636c63325ab5f704212
theenuka <theenukabandara@gmail.com>
2025-01-12 16:51:51 +0530

    admin file added
    admin files added

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoute=require("./routes/auth")
+const userRoute=require("./routes/user")
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoute)
+app.use("/users", userRoute)
 
 const DB = process.env.DB; // Ensure this matches the variable in your .env file
 const PORT = process.env.PORT || 5000;
