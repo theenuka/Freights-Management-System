3ae8f04118d7bcca815350cb859efa07c5f61200
theenuka <theenukabandara@gmail.com>
2025-09-09 03:24:25 +0530

    email files changed

--- BackgroundServices/EmailService/DeliveredParcel.js
+++ BackgroundServices/EmailService/DeliveredParcel.js
@@ -4,68 +4,54 @@ const sendMail = require("../helpers/sendMail");
 const Parcel = require("../models/Parcel");
 dotenv.config();
 
+// Helper to send email - helper function
+const sendParcelEmail = async (parcel, recipientEmail) => {
+  ejs.renderFile(
+    "templates/deliveredparcel.ejs",
+    {
+      sendername: parcel.sendername,
+      from: parcel.from,
+      to: parcel.to,
+      recipientname: parcel.recipientname,
+      cost: parcel.cost,
+      weight: parcel.weight,
+      note: parcel.note,
+    },
+    async (err, data) => {
+      if (err) {
+        console.log("EJS render error:", err);
+        return;
+      }
+
+      const messageOption = {
+        from: process.env.EMAIL,
+        to: recipientEmail,
+        subject: "Your parcel has been delivered",
+        html: data,
+      };
+
+      try {
+        await sendMail(messageOption);
+      } catch (error) {
+        console.log("Mail sending error:", error);
+      }
+    }
+  );
+};
+
 const SendParcelDeliveredEmail = async () => {
   const parcels = await Parcel.find({ status: 2 });
 
-  if (parcels.length > 0) {
-    for (let parcel of parcels) {
-      ejs.renderFile(
-        "templates/deliveredparcel.ejs",
-        {
-          sendername: parcel.sendername,
-          from: parcel.from,
-          to: parcel.to,
-          recipientname: parcel.recipientname,
-          cost: parcel.cost,
-          weight: parcel.weight,
-          note: parcel.note,
-        },
-        async (err, data) => {
-          let messageoption = {
-            from: process.env.EMAIL,
-            to: parcel.senderemail,
-            subject: "Your parcel has been delivered",
-            html: data,
-          };
+  for (let parcel of parcels) {
+    // Send email to sender
+    await sendParcelEmail(parcel, parcel.senderemail);
 
-          try {
-            sendMail(messageoption);
-           
-          } catch (error) {
-            console.log(err);
-          }
-        }
-      );
+    // Send email to recipient
+    await sendParcelEmail(parcel, parcel.recipientemail);
 
-      ejs.renderFile(
-        "templates/deliveredparcel.ejs",
-        {
-          sendername: parcel.sendername,
-          from: parcel.from,
-          to: parcel.to,
-          recipientname: parcel.recipientname,
-          cost: parcel.cost,
-          weight: parcel.weight,
-          note: parcel.note,
-        },
-        async (err, data) => {
-          let messageoption = {
-            from: process.env.EMAIL,
-            to: parcel.recipientemail,
-            subject: "Your parcel has been delivered",
-            html: data,
-          };
-
-          try {
-            sendMail(messageoption);
-            await Parcel.findByIdAndUpdate(parcel._id, { $set: { status: 3} });
-          } catch (error) {
-            console.log(err);
-          }
-        }
-      );
-    }
+    // Update parcel status to 3 (delivered email sent)
+    await Parcel.findByIdAndUpdate(parcel._id, { $set: { status: 3 } });
   }
 };
 
-module.exports = {SendParcelDeliveredEmail };
+module.exports = { SendParcelDeliveredEmail };
--- BackgroundServices/EmailService/WelcomeEmail.js
+++ BackgroundServices/EmailService/WelcomeEmail.js
@@ -6,34 +6,39 @@ const CryptoJs = require("crypto-js");
 
 dotenv.config();
 
-const sendWelcomeEmail = async (fullname, password, email) => {
+const sendWelcomeEmail = async () => {
   const users = await User.find({ status: 0 });
- 
 
-  if (users.length > 0) {
-    for (let user of users) {
-      const hashedPassword = CryptoJs.AES.decrypt(user.password, process.env.PASS);
-      const originalPassword = hashedPassword.toString(CryptoJs.enc.Utf8);
-      ejs.renderFile(
-        "templates/welcome.ejs",
-        { fullname: user.fullname,password:originalPassword, email: user.email },
-        async (err, data) => {
-          let messageoption = {
-            from: process.env.EMAIL,
-            to: user.email,
-            subject: "Welcome to FMs",
-            html: data,
-          };
+  for (let user of users) {
+    // Decrypt the password
+    const hashedPassword = CryptoJs.AES.decrypt(user.password, process.env.PASS);
+    const originalPassword = hashedPassword.toString(CryptoJs.enc.Utf8);
 
-          try {
-            sendMail(messageoption);
-            await User.findByIdAndUpdate(user._id, { $set: { status: 1 } });
-          } catch (error) {
-            console.log(err);
-          }
+    // Render the email template
+    ejs.renderFile(
+      "templates/welcome.ejs",
+      { fullname: user.fullname, password: originalPassword, email: user.email },
+      async (err, data) => {
+        if (err) {
+          console.log("EJS render error:", err);
+          return;
         }
-      );
-    }
+
+        const messageOption = {
+          from: process.env.EMAIL,
+          to: user.email,
+          subject: "Welcome to FMS",
+          html: data,
+        };
+
+        try {
+          await sendMail(messageOption); // Ensure email is sent before updating status
+          await User.findByIdAndUpdate(user._id, { $set: { status: 1 } });
+        } catch (error) {
+          console.log("Mail sending error:", error);
+        }
+      }
+    );
   }
 };
 
